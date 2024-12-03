6fd8eb8fe6b82fc40dc169ce11be43a790464faa
Krishna-aa <krishnaanand0001@gmail.com>
2024-12-03 19:48:36 +0530

    modified personla data, updated picture

--- src/app/services/employeement-data/employeement-data.service.ts
+++ src/app/services/employeement-data/employeement-data.service.ts
@@ -1,28 +1,33 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeementDataService {
-  employeementData=[
+  employeementData = [
     {
       id: 1,
-      title: 'Product Engineer Intern',
-      company: "Tezo Dgital",
-      duration: "(Jan 2024 - Present)"
+      title: 'Software Engineer L2',
+      company: 'Predigle',
+      duration: '(November - Present)',
     },
     {
       id: 2,
-      title: "Java Backend Develoepr",
-      company: "HighRadius",
-      duration: "(July 2023 - Nov 2024)"
+      title: 'Product Engineer Intern',
+      company: 'Tezo Dgital',
+      duration: '(Jan 2024 - July 2024)',
     },
     {
       id: 3,
-      title: "Content Creator",
-      company: "Unacademy",
-      duration: "(Sept 2020 - Sept2021)"
-    }
+      title: 'Java Backend Develoepr',
+      company: 'HighRadius',
+      duration: '(July 2023 - Nov 2024)',
+    },
+    {
+      id: 4,
+      title: 'Content Creator',
+      company: 'Unacademy',
+      duration: '(Sept 2020 - Sept2021)',
+    },
   ];
-
 }
--- src/app/services/personal-data/personal-data.service.ts
+++ src/app/services/personal-data/personal-data.service.ts
@@ -9,7 +9,7 @@ export class PersonalDataService {
     phone: '+91 7004155601',
     email:  'krishnaanand0001@gmail.com',
     address: 'Madhapur, Hyderabad, Telangana, India',
-    profile:'assets/image/profile.png',
+    profile:'assets/image/profile.jpg',
     designation: 'Software Developer',
     description: "I am Krishna Anand, a dedicated and enthusiastic programmer with a strong passion for learning and exploring new technologies. My expertise lies in problem-solving and web application development, with a core focus on JavaScript, which I use across multiple development stacks. I am a quick learner with a self-driven approach, always eager to expand my knowledge and contribute to making the web more accessible and dynamic. I am currently open to job opportunities that align with my skills and professional interests.",
     github: 'https://github.com/Krishn-aa',
